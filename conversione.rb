f65d600776986716774d43eb7108e3f068e335a1
Carmine Zaccagnino <carmine@pc-carmine.localdomain>
2017-10-05 17:27:47 +0200

    funziona quasi perfettamente

--- conversione.rb
+++ conversione.rb
@@ -1,4 +1,36 @@
 #!/usr/bin/env ruby
+def indec(nbin)
+  nout=0
+  i=0
+  numerocorrente=nbin
+  while numerocorrente>0
+    nout=nout+numerocorrente%10*2**i
+    i=i+1
+    numerocorrente=Integer(numerocorrente/10)
+  end
+  return nout
+end
+def inbin(ndec)
+  resti=0
+  contatore=1
+  numerocorrente=ndec
+  while numerocorrente>0
+    resti=resti+numerocorrente%2*contatore
+    contatore=contatore*10
+    numerocorrente=Integer(numerocorrente/2)
+  end
+  return resti
+end
+def conversione(nin,s)
+  if s=="decbin"
+    puts "DECBIN!"
+    return inbin(nin)
+  else
+    puts "BINDEC!"
+    return indec(nin)
+  end
+end
+
 def main()
   puts "Vuoi convertire da binario a decimale(bindec) o da decimale a binario(decbin)?"
   scelta=gets.chomp
@@ -9,9 +41,12 @@ def main()
   end
   puts "Hai scritto "+'"'+scelta+'"'
   puts "Inserisci un numero da convertire"
-  numero=gets.chomp.to_i
+  numeroinput=gets.chomp.to_i
   print "Hai inserito il numero "
-  puts numero
+  puts numeroinput
+  numerooutput=conversione(numeroinput,scelta)
+  print "E convertito è "
+  puts numerooutput
   puts "VUOI COMINCIARE DA CAPO IL PROGRAMMA?"
   print "(SI O NO?)"
   fine=gets.chomp
